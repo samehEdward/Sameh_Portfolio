ada9067f6e109d99317133c3038d65bcad5e7958
Sameh Edward <edw21629@spengergasse.at>
2023-03-07 23:33:43 +0100

    Components aktualisiert

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from "react";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
