dfda3f4942d6defc0cc13f4c3fe88d15373bccb3
Sameh Edward <edw21629@spengergasse.at>
2023-03-07 15:43:57 +0100

    Frontend

--- Backend/schemas/index.js
+++ Backend/schemas/index.js
@@ -1,3 +1,19 @@
+import skills from './skills'
 import testimonial from './testimonial'
+import brands from './brands'
+import contact from './contact'
+import abouts from './abouts'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import works from './works'
 
-export const schemaTypes = [testimonial]
+export const schemaTypes = [
+  testimonial,
+  abouts,
+  skills,
+  brands,
+  contact,
+  workExperience,
+  works,
+  experiences,
+]
