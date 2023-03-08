b80a4bc0e337172f16d00f8b0e1fe96d45b0a1fc
Sameh Edward <edw21629@spengergasse.at>
2023-03-08 14:32:47 +0100

    Navbar Design

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Navbar.scss";
+import { images } from "../../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="app__navbar">
+        <div className="app__navbar-logo">
+          <img src={images.logo} alt="Logo" />
+        </div>
+        <ul className="app__navbar-links">
+          {["home", "about", "work", "skills", "contact"].map((item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div></div>
+              <a href={`#{item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ frontend_react/src/constants/index.js
@@ -0,0 +1,5 @@
+// import images from "./images";
+
+// export { images };
+
+export { default as images } from "./images";
